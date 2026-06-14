157d28f93f6a45525a477b906cce28b21d2c3aa2
Tristan Wang <80198655+TristanWH@users.noreply.github.com>
2026-06-14 20:01:17 +0800

    Delete saegd_net_project directory

--- saegd_net_project/saegdnet/__init__.py
+++
@@ -1,3 +0,0 @@
-"""SAEGD-Net package."""
-
-__version__ = "0.1.0"
--- saegd_net_project/tests/test_smoke_imports.py
+++
@@ -1,5 +0,0 @@
-def test_imports():
-    import saegdnet
-    from saegdnet.models.saegd import build_model
-    from saegdnet.diffusion.gaussian import GaussianDiffusion
-    assert saegdnet.__version__
